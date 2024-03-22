dbac47d256b5859d0c855e6ea277b2d8b39a9ed1
mohammed shibili cv <mohshibilicv@gmail.com>
2024-03-22 19:30:54 +0530

    Fix: Allowed Hosts Bug

--- brohealth/settings.py
+++ brohealth/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["https://brohealth-backend.onrender.com", "http://brohealth-backend.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["brohealth-backend.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
